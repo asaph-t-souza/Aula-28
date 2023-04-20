62cee52d6975f2593a6b24339c351c7a049299af
asaph-t-Souza <asaph.net@gmail.com>
2023-04-20 19:54:21 -0300

    1

---
+++ spring-one-to-one/src/main/java/com/t3/springonetoone/entity/Character.java
@@ -0,0 +1,29 @@
+package com.t3.springonetoone.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity // Define a classe como uma entidade
+@Table(name="characters")
+@Getter @Setter @NoArgsConstructor // Modo Preguiça
+public class Character {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "character_level")
+    private int characterLevel;
+
+    @Column(name = "character_class")
+    private String characterClass;
+
+    //private Pet pet;
+    
+}
