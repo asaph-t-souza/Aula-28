e05be4d142287c4065dc63988931f7c4ee2781c7
asaph-t-Souza <asaph.net@gmail.com>
2023-04-20 19:25:05 -0300

    init

---
+++ spring-one-to-one/src/main/java/com/t3/springonetoone/SpringOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.t3.springonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOneToOneApplication.class, args);
+	}
+
+}
---
+++ spring-one-to-one/src/test/java/com/t3/springonetoone/SpringOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t3.springonetoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
