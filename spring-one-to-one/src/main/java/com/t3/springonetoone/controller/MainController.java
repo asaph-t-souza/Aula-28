dddf1b70c86ec7a641d4c68ccbb519c44736f4a5
asaph-t-Souza <asaph.net@gmail.com>
2023-04-20 21:57:54 -0300

    aula 28

--- spring-one-to-one/src/main/java/com/t3/springonetoone/controller/MainController.java
+++ spring-one-to-one/src/main/java/com/t3/springonetoone/controller/MainController.java
@@ -5,23 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.t3.springonetoone.entity.Person;
+import com.t3.springonetoone.entity.Pet;
 import com.t3.springonetoone.repository.PersonRepository;
 
 @Controller
 public class MainController {
 
     @Autowired
-    private PersonRepository charRepo;
+    private PersonRepository personRepo;
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
-    @GetMapping("/characters/new")
-    public String createCharacter(Model model) {
+    @GetMapping("/persons/new")
+    public String createPerson(Model model) {
+        Person person = new Person(); //Cria uma pessoa
+        Pet pet = new Pet(); //Cria um pet
+        person.setPetId(pet); //Vincula o pet a pessoa
 
-        return "addEditChar";
+        model.addAttribute("person", person);
+        return "addEditPerson";
     }
 
 }
