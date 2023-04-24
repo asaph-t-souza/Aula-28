ea26486114449857247d5c0485e329cfcef82d6e
asaph-t-Souza <asaph.net@gmail.com>
2023-04-24 20:11:58 -0300

    aula 29

--- spring-one-to-one/src/main/java/com/t3/springonetoone/controller/MainController.java
+++ spring-one-to-one/src/main/java/com/t3/springonetoone/controller/MainController.java
@@ -1,19 +1,26 @@
 package com.t3.springonetoone.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.t3.springonetoone.entity.Person;
 import com.t3.springonetoone.entity.Pet;
 import com.t3.springonetoone.repository.PersonRepository;
+import com.t3.springonetoone.repository.PetRepository;
 
 @Controller
 public class MainController {
 
     @Autowired
     private PersonRepository personRepo;
+
+    @Autowired
+    private PetRepository petRepo;
 
     @GetMapping("/")
     public String index() {
@@ -28,6 +35,26 @@ public class MainController {
 
         model.addAttribute("person", person);
         return "addEditPerson";
+    }
+
+    @PostMapping("/persons/save")
+    public String savePerson(Person person){
+        personRepo.save(person); // Cascade.ALL salva ambas entidades
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/persons")
+    public String getAllPersons(Model model){
+        List<Person> persons = personRepo.findAll();
+        model.addAttribute("persons", persons);
+        return "PersonList";
+    }
+
+    @GetMapping("/pets")
+    public String getAllPets(Model model){
+        List<Pet> pets = petRepo.findAll();
+        model.addAttribute("pets", pets);
+        return "PetList";
     }
 
 }
